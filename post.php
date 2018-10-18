c9bfc3031087bbb87cca532cd6d977d589d737d3
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-10-18 13:25:27 -1000

    lesson 128 finished

--- post.php
+++ post.php
@@ -69,7 +69,22 @@
 
                 <?php 
                 if(isset($_POST['create_comment'])){
-                    echo $_POST['comment_author'];
+
+                    $the_post_id = $_GET['p_id'];
+
+                    $comment_author = $_POST['comment_author'];
+                    $comment_email = $_POST['comment_email'];
+                    $comment_content = $_POST['comment_content'];
+
+                    $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
+                    $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now()) ";
+
+                    $create_comment_query = mysqli_query($connection, $query);
+
+                    if(!$create_comment_query){
+                        die('Query Failed ' . mysqli_error($connection));
+                    }
+
                 }
                 
                 ?>
