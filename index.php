e73757c3f15170e34860d957edf3dcac701d587e
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-10-19 09:12:25 -1000

    lesson 135 finished

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
                 <?php
 
                 
-                $query = "SELECT * FROM posts";
+                $query = "SELECT * FROM posts ";
                 $select_all_posts_query = mysqli_query($connection, $query);
 
                 while($row = mysqli_fetch_assoc($select_all_posts_query)) {
@@ -30,6 +30,17 @@
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
                     $post_content = substr($row['post_content'], 0, 100);
+                    $post_status = $row['post_status'];
+
+                    if($post_status !== 'published'){
+
+                        echo "<h1 class='text-center'>NO POST SORRY</h1>";
+
+                    } else {
+
+
+
+                    
 
                     ?>
 
@@ -59,7 +70,7 @@
 
 
                     
-                 <?php   }  ?>
+                 <?php   }  }  ?>
 
 
 
