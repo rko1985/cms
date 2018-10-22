c9c1752da83e5edb1863203c24fa6881c4b633e4
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-10-22 08:55:27 -1000

    lesson 150, users section finished

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -1,78 +1,74 @@
 <?php 
 
-    if(isset($_POST['create_post'])){
+    if(isset($_POST['create_user'])){
 
-        $post_title = $_POST['title'];
-        $post_author = $_POST['author'];
-        $post_category_id = $_POST['post_category'];
-        $post_status = $_POST['post_status'];
+        $user_firstname = $_POST['user_firstname'];
+        $user_lastname = $_POST['user_lastname'];
+        $user_role = $_POST['user_role'];
 
-        $post_image = $_FILES['image']['name'];
-        $post_image_temp = $_FILES['image']['tmp_name'];
+        // $post_image = $_FILES['image']['name'];
+        // $post_image_temp = $_FILES['image']['tmp_name'];
 
-        $post_tags = $_POST['post_tags'];
-        $post_content = $_POST['post_content'];
-        $post_date = date('d-m-y');
+        $username = $_POST['username'];
+        $user_email = $_POST['user_email'];
+        $user_password = $_POST['user_password'];
+        // $post_date = date('d-m-y');
 
-        move_uploaded_file($post_image_temp, "../images/$post_image");
+        // move_uploaded_file($post_image_temp, "../images/$post_image");
 
-        $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status) ";
-        $query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}',now(), '{$post_image}','{$post_content}','{$post_tags}', '{$post_status}' ) ";
+        $query = "INSERT INTO users(user_firstname, user_lastname, user_role, username, user_email, user_password) ";
+        $query .= "VALUES('{$user_firstname}','{$user_lastname}', '{$user_role}','{$username}','{$user_email}', '{$user_password}' ) ";
 
-        $create_post_query = mysqli_query($connection, $query);
+        $create_user_query = mysqli_query($connection, $query);
 
-        confirmQuery($create_post_query);
+        confirmQuery($create_user_query);
     }
 
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
+    
     <div class="form-group">
-        <label for="title">Post Title</label>
-        <input type="text" class="form-control" name="title">
+        <label for="title">Firstname</label>
+        <input type="text" class="form-control" name="user_firstname">
     </div>
     <div class="form-group">
-        <select name="post_category" id="post_category">
-            <?php 
-            
-            $query = "SELECT * FROM categories ";
-            $select_categories = mysqli_query($connection, $query);
+        <label for="post_status">Lastname</label>
+        <input type="text" class="form-control" name="user_lastname">
+    </div>
 
-            confirmQuery($select_categories);
+    <div class="form-group">
+        <select name="user_role" id="">
+        <option value="subscriber">Select Options</option>
+            <option value="admin">Admin</option>
+            <option value="subscriber">Subscriber</option>
+        </select>    
+    </div>
 
-            while($row = mysqli_fetch_assoc($select_categories)) {
-            $cat_id = $row['cat_id'];
-            $cat_title = $row['cat_title'];
-            
-            echo "<option value='{$cat_id}'>{$cat_title}</option>";
-            
-            }
 
-            
-            ?>        
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="title">Post Author</label>
-        <input type="text" class="form-control" name="author">
-    </div>
-    <div class="form-group">
-        <label for="post_status">Post Status</label>
-        <input type="text" class="form-control" name="post_status">
-    </div>
-    <div class="form-group">
+
+
+
+    <!-- <div class="form-group">
         <label for="post_image">Post Image</label>
         <input type="file" name="image">
+    </div> -->
+
+
+    <div class="form-group">
+        <label for="post_tags">Username</label>
+        <input type="text" class="form-control" name="username">
     </div>
     <div class="form-group">
-        <label for="post_tags">Post Tags</label>
-        <input type="text" class="form-control" name="post_tags">
+        <label for="post_content">Email</label>
+        <input type="email" class="form-control" name="user_email">
+    </div>
+
+    <div class="form-group">
+        <label for="post_content">Password</label>
+        <input type="password" class="form-control" name="user_password">
     </div>
     <div class="form-group">
-        <label for="post_content">Post Content</label>
-        <textarea class="form-control" name="post_content" id="" cols="30" rows="10"></textarea>
-    </div>
-    <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+        <input class="btn btn-primary" type="submit" name="create_user" value="Add User">
     </div>
 </form>
--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -25,7 +25,7 @@
             $user_image = $row['user_image'];
             $user_role = $row['user_role'];
             
-            echo "<tr>";
+   
             echo "<td>$user_id</td>";
             echo "<td>$username</td>";
             echo "<td>$user_firstname</td>";
@@ -58,14 +58,11 @@
             // }
 
 
-            
 
-
-
-            echo "<td></td>";  
-            echo "<td><a href='comments.php?approve='>Approve</a></td>";    
-            echo "<td><a href='comments.php?unapprove='>Unapprove</a></td>"; 
-            echo "<td><a href='comments.php?delete='>Delete</a></td>";                                            
+            echo "<td><a href='users.php?change_to_admin={$user_id}'>Admin</a></td>";    
+            echo "<td><a href='users.php?change_to_sub={$user_id}'>Subscriber</a></td>";
+            echo "<td><a href='users.php?source=edit_user&edit_user={$user_id}'>Edit</a></td>";      
+            echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";                                            
             echo "</tr>";
                                         
         }
@@ -79,25 +76,24 @@
 
 <?php 
 
-if(isset($_GET['approve'])){
+if(isset($_GET['change_to_admin'])){
 
-    $the_comment_id = $_GET['approve'];
+    $the_user_id = $_GET['change_to_admin'];
 
-    $query = "UPDATE comments SET comment_status = 'approved' WHERE comment_id = $the_comment_id ";
-    $approve_comment_query = mysqli_query($connection, $query);
-    header("Location: comments.php");
+    $query = "UPDATE users SET user_role = 'admin' WHERE user_id = $the_user_id ";
+    $change_to_admin_query = mysqli_query($connection, $query);
+    header("Location: users.php");
 
 
 }
 
-if(isset($_GET['unapprove'])){
+if(isset($_GET['change_to_sub'])){
 
-    $the_comment_id = $_GET['unapprove'];
+    $the_user_id = $_GET['change_to_sub'];
 
-    $query = "UPDATE comments SET comment_status = 'unapproved' WHERE comment_id = $the_comment_id ";
-    $unapprove_comment_query = mysqli_query($connection, $query);
-    header("Location: comments.php");
-
+    $query = "UPDATE users SET user_role = 'subscriber' WHERE user_id = $the_user_id ";
+    $change_to_sub_query = mysqli_query($connection, $query);
+    header("Location: users.php");
 
 }
 
@@ -105,11 +101,11 @@ if(isset($_GET['unapprove'])){
 
 if(isset($_GET['delete'])){
 
-    $the_comment_id = $_GET['delete'];
+    $the_user_id = $_GET['delete'];
 
-    $query = "DELETE FROM comments WHERE comment_id = {$the_comment_id}";
-    $delete_query = mysqli_query($connection, $query);
-    header("Location: comments.php");
+    $query = "DELETE FROM users WHERE user_id = {$the_user_id}";
+    $delete_user_query = mysqli_query($connection, $query);
+    header("Location: users.php");
 
 
 }
