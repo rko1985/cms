2018b77dd0f0af63f58815363e8d3b36afe0967d
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-10-29 11:44:25 -1000

    bulk media select working lesson 184 finished

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -10,5 +10,25 @@ $(document).ready(function(){
 
     //Rest of the code here
 
+    //Bulk Media Selecting (select all boxes)
+   
+    $('#selectAllBoxes').click(function(event){
+
+        if(this.checked){
+            $('.checkBoxes').each(function(){
+
+                this.checked = true;
+
+            });
+        } else {
+            $('.checkBoxes').each(function(){
+
+                this.checked = false;
+
+            });
+        }
+
+    });
+
 });
 
