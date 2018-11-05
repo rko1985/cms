465d4b3953c8871f74bec239e2c983c2554b46af
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-11-05 10:35:41 -1000

    added pagination lesson 216 finished

--- index_old.php
+++ index_old.php
@@ -18,39 +18,9 @@
             <div class="col-md-8">
 
                 <?php
-                //Pagination (also look near footer for UL)
 
-                $per_page = 5;
-
-                if(isset($_GET['page'])){
-
-                    $page = $_GET['page'];
-
-                } else {
-
-                    $page = "";
-
-                }
-
-                if($page == "" || $page == 1) {
-
-                    $page_1 = 0;
-
-                } else {
-
-                    $page_1 = ($page * $per_page) - $per_page;
-
-                }
-
-                $post_query_count = "SELECT * FROM posts"; //finding count of all posts
-                $find_count = mysqli_query($connection, $post_query_count);
-                $count = mysqli_num_rows($find_count);
                 
-                $count = ceil($count / $per_page); //finding number of pages by dividing post count/number of pages per post (counts and rounds up)
-
-                $query = "SELECT * FROM posts  LIMIT $page_1, $per_page"; 
-                //End of pagination
-
+                $query = "SELECT * FROM posts ";
                 $select_all_posts_query = mysqli_query($connection, $query);
 
                 while($row = mysqli_fetch_assoc($select_all_posts_query)) {
@@ -73,8 +43,6 @@
                 </h1>
 
                 <!-- First Blog Post -->
-
-                <h1><?php echo $count; ?></h1>
                 <h2>
                     <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                 </h2>
@@ -114,28 +82,5 @@
         <!-- /.row -->
 
         <hr>
-
-        <!-- Pagination UL-->             
-        <ul class="pager">
-            <?php 
-            
-            for($i = 1 ; $i <= $count ; $i++){ 
-
-                if($i == $page) { //checks if current page then style the link active
-
-                    echo "<li><a class='active_link' href='index.php?page={$i}'>{$i}</a></li>";
-
-                } else {
-
-                    echo "<li><a href='index.php?page={$i}'>{$i}</a></li>";
-
-                }
-
-                
-
-            }
-
-            ?>
-        </ul> <!-- End of Pagination -->
 
 <?php include("includes/footer.php"); ?>
