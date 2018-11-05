81022b341e9f34b656d9251f5ec389c5afae0190
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-11-05 08:45:07 -1000

    added post cloning, loader and view counter/reset, lesson 211 finished

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -30,5 +30,16 @@ $(document).ready(function(){
 
     });
 
+    //Loader
+
+    var div_box = "<div id='load-screen'><div id='loading'></div></div>";
+
+    $("body").prepend(div_box);
+
+    $('#load-screen').delay(700).fadeOut(600, function(){
+        $(this).remove();
+    });
+
+
 });
 
